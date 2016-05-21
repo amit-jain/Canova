ad2d71f27e68fb266e2eba22bc72ab5d3cd7d00b
Alex Black <blacka101@gmail.com>
2016-05-21 23:26:32 +1000

    Add Spark LineRecordReaderFunction

---
+++ canova-spark/src/main/java/org/canova/spark/functions/LineRecordReaderFunction.java
@@ -0,0 +1,27 @@
+package org.canova.spark.functions;
+
+import lombok.AllArgsConstructor;
+import org.apache.spark.api.java.function.Function;
+import org.canova.api.records.reader.RecordReader;
+import org.canova.api.split.StringSplit;
+import org.canova.api.writable.Writable;
+
+import java.util.Collection;
+
+/**
+ * LineRecordReaderFunction: Used to map a {@code JavaRDD<String>} to a {@code JavaRDD<Collection<Writable>>}
+ * Note that this is most useful with LineRecordReader instances (CSVRecordReader, SVMLightRecordReader, etc)
+ *
+ * @author Alex Black
+ */
+@AllArgsConstructor
+public class LineRecordReaderFunction implements Function<String,Collection<Writable>> {
+
+    private final RecordReader recordReader;
+
+    @Override
+    public Collection<Writable> call(String s) throws Exception {
+        recordReader.initialize(new StringSplit(s));
+        return recordReader.next();
+    }
+}
