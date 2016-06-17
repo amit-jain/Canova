516ae623eeebd201764059dfed9bc52fa606d4ff
Samuel Audet <samuel.audet@gmail.com>
2016-06-17 23:16:34 +0900

    Prevent images from being deallocated while still accessing them

--- canova-nd4j/canova-nd4j-image/src/main/java/org/canova/image/loader/NativeImageLoader.java
+++ canova-nd4j/canova-nd4j-image/src/main/java/org/canova/image/loader/NativeImageLoader.java
@@ -270,6 +270,7 @@ public class NativeImageLoader extends BaseImageLoader {
                 }
             }
         }
+        image.deallocate();
         if (normalizeIfNeeded) {
             ret = normalizeIfNeeded(ret);
         }
